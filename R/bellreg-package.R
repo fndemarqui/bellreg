e2de961e8620848d8ff3b48b66cfd770e293b75a
fndemarqui <fndemarqui@gmail.com>
2024-01-16 04:34:37 -0300

    Initial commit

---
+++ R/bellreg-package.R
@@ -0,0 +1,16 @@
+#' The 'bellreg' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name bellreg-package
+#' @aliases bellreg
+#' @useDynLib bellreg, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.5. https://mc-stan.org
+#'
+NULL
